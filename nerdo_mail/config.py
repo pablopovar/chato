aa0801040cb696934cc751a886a9718dc9f57db1
pablopovar <pablo@povarchik.com>
2026-07-27 22:10:13 -0400

    Configure mailbox domain operations API client

--- nerdo_mail/config.py
+++ nerdo_mail/config.py
@@ -24,6 +24,8 @@ class MailSettings:
     users_dir: Path = Path(os.getenv("NERDO_USERS_DIR", "/app/users"))
     core_base_url: str = os.getenv("NERDO_CORE_BASE_URL", "http://nerdo:3401").rstrip("/")
     core_admin_token: str = os.getenv("NERDO_CORE_ADMIN_TOKEN", "")
+    gateway_base_url: str = os.getenv("NERDO_GATEWAY_BASE_URL", "http://nerdo-api:3400").rstrip("/")
+    operator_token: str = os.getenv("NERDO_OPERATOR_TOKEN", "")
     poll_seconds: float = float(os.getenv("NERDO_MAIL_POLL_SECONDS", "10"))
     stable_seconds: float = float(os.getenv("NERDO_MAIL_STABLE_SECONDS", "2"))
     admin_emails: frozenset[str] = _emails("NERDO_ADMIN_EMAILS")
